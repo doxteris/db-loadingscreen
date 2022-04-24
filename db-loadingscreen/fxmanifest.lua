ca1846fc6e95751de8e1d203f8dec1ce1d5802bb
doxteris <83948587+doxteris@users.noreply.github.com>
2022-04-24 22:22:43 +0300

    Files

---
+++ db-loadingscreen/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author "dbScripts"
+description "Modern loadingscreen for FiveM server"
+
+files {
+    'index.html',
+    'main.css',
+    'fonts/Gilroy-Light.otf',
+    'img/background.png',
+    'music/song.mp3',
+    'main.js'
+}
+
+loadscreen 'index.html'
